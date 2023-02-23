83975e4bcd7e43fccf558dc6d7ef85f1ff6e1c92
jorjettamkryan <jorjettamkryan@gmail.com>
2023-02-23 18:42:26 +0400

    calculators

---
+++ colculaator.py
@@ -0,0 +1,40 @@
+def sum_numbers(a: int, b: int) -> int:
+    return a + b
+
+
+def divide_numbers(a: int, b: int) -> float:
+    return a / b
+
+
+def sub_numbers(a: int, b: int) -> int:
+    return a - b
+
+
+def mult_numbers(a:int, b: int) -> int:
+    return a * b
+
+
+def power_numbers(a: int,b: int) -> int:
+    c = a
+    for i in range(b - 1):
+        c = c * a
+    return c
+
+
+first_input = input("number")
+action_input = input("action")
+last_input = input("number")
+print(input, action_input, last_input)
+
+if action_input == '+':
+    result = sum_numbers(int(first_input),int(last_input))
+elif action_input == '/':
+    result = divide_numbers(int(first_input), int(last_input))
+elif action_input == '-':
+    result = sub_numbers(int(first_input), int(last_input))
+elif action_input == '*':
+    result =mult_numbers(int(first_input), int(last_input))
+elif action_input == '**':
+    result = power_numbers(int(first_input), int(last_input))
+print(result)
+
